4ba8edde8d2de83ef7858244b00d2860f80e9951
PletnevaMA <mariya.pletneva@simbirsoft.com>
2022-08-10 11:26:39 +0400

    Реализована подсветка доступных ходов и перемещение фигур по доске

--- chess/src/components/CellComponent.tsx
+++ chess/src/components/CellComponent.tsx
@@ -3,10 +3,17 @@ import { Cell } from "../models/Cell";
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
-const CellComponent: FC<CellProps> = ({ cell }) => {
+const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
   return (
-    <div className={["cell", cell.color].join(" ")}>
+    <div
+      className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+      onClick={() => click(cell)}
+      style={{ background: cell.avaliable && cell.figure ? "green" : "" }}
+    >
+      {!cell.figure && cell.avaliable && <div className="avaliable"></div>}
       {cell.figure?.logo && (
         <img alt={String(cell.figure)} src={cell.figure.logo} />
       )}
